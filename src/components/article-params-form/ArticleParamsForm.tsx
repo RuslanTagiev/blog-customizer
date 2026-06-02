cf77142c6460f16fb706b2d52f06e1c7505bb5df
Ruslan Tagiev <tagiev830@gmail.com>
2026-06-02 18:22:57 +0300

    Выполнил первый шаг. реализовал открытие панели и добавил выбор шрифта

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,44 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-
+import { useState, useRef } from 'react';
+import {
+	defaultArticleState,
+	fontFamilyOptions,
+} from 'src/constants/articleProps';
+import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
+import clsx from 'clsx';
 import styles from './ArticleParamsForm.module.scss';
+import { Select } from 'src/ui/select';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [formState, setFormState] = useState(defaultArticleState);
+	const formRef = useRef<HTMLDivElement>(null);
+	const toggleMenu = () => {
+		setIsOpen((prev) => !prev);
+	};
+
+	useOutsideClickClose({
+		isOpen,
+		rootRef: formRef,
+		onChange: setIsOpen,
+	});
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={toggleMenu} />
+			<aside
+				ref={formRef}
+				className={clsx(styles.container, isOpen && styles.container_open)}>
 				<form className={styles.form}>
+					<Select
+						title='Шрифт'
+						options={fontFamilyOptions}
+						selected={formState.fontFamilyOption}
+						onChange={(selected) =>
+							setFormState({ ...formState, fontFamilyOption: selected })
+						}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
