60307c75109164d617b59e7021b625318768c6de
Ruslan Tagiev <tagiev830@gmail.com>
2026-06-03 22:41:27 +0300

    выполнил шаг 2.  добавил  все элементы управления в форму параметров статьи

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -4,11 +4,16 @@ import { useState, useRef } from 'react';
 import {
 	defaultArticleState,
 	fontFamilyOptions,
+	fontSizeOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
 } from 'src/constants/articleProps';
 import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
 import clsx from 'clsx';
 import styles from './ArticleParamsForm.module.scss';
 import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
 
 export const ArticleParamsForm = () => {
 	const [isOpen, setIsOpen] = useState(false);
@@ -39,6 +44,40 @@ export const ArticleParamsForm = () => {
 							setFormState({ ...formState, fontFamilyOption: selected })
 						}
 					/>
+					<RadioGroup
+						title='Размер шрифта'
+						name='fontSize'
+						options={fontSizeOptions}
+						selected={formState.fontSizeOption}
+						onChange={(selected) =>
+							setFormState({ ...formState, fontSizeOption: selected })
+						}
+					/>
+					<Select
+						title='Цвет шрифта'
+						options={fontColors}
+						selected={formState.fontColor}
+						onChange={(selected) =>
+							setFormState({ ...formState, fontColor: selected })
+						}
+					/>
+					<Select
+						title='Цвет фона'
+						options={backgroundColors}
+						selected={formState.backgroundColor}
+						onChange={(selected) =>
+							setFormState({ ...formState, backgroundColor: selected })
+						}
+					/>
+					<Select
+						title='Ширина контента'
+						options={contentWidthArr}
+						selected={formState.contentWidth}
+						onChange={(selected) =>
+							setFormState({ ...formState, contentWidth: selected })
+						}
+					/>
+
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
